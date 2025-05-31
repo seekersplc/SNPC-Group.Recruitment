3cae74e430d6d83241f540902f81942e83f13498
seekersplc <bigmell41@gmail.com>
2025-05-31 19:52:39 +0100

    Create sn.js

---
+++ sn.js
@@ -0,0 +1,5 @@
+var password = 'SNPC GROUP'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement, cliquez sur OK pour démarrer la candidature  ','SNPC GROUP');
+if (password != 'SNPC GROUP') {
+location.href='https://seekersplc.github.io/SNPC-Group.Recruitment';
+}
